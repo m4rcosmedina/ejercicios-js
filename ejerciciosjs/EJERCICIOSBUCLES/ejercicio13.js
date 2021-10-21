be547af5ea5a3ce7f0aee878a09c82889e6b2157
marcos <marcosjmedina@gmail.com>
2021-10-21 20:21:51 -0300

    ejercicio 13 listo

---
+++ ejerciciosjs/EJERCICIOSBUCLES/ejercicio13.js
@@ -0,0 +1,7 @@
+
+//13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+let txt = prompt('Por favor, Ingrese un texto')
+txt = txt.toUpperCase()
+
+document.write('Su texto en mayusculas es el siguiente ='+ txt);

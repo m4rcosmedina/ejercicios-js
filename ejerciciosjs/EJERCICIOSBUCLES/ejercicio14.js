be1825930a20de4149797081c2feaf53b2d75ff2
marcos <marcosjmedina@gmail.com>
2021-10-21 22:00:23 -0300

    ejercicio 14

--- ejerciciosjs/EJERCICIOSBUCLES/ejercicio14.js
+++ ejerciciosjs/EJERCICIOSBUCLES/ejercicio14.js
@@ -1,1 +1,12 @@
-//14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
+/*14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – 
+entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”,
+ deberá salir “h-o-l-a- -q-u-e- -t-a-l”.*/
+
+
+ let frase = prompt('ingrese una frase');
+
+
+ for (let caracter = 0; caracter < frase.length; caracter++) {
+     document.write(frase.substr(caracter,1) + '-');
+ } 
+
